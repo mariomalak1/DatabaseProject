b67cef3e1a36be11c80f4c2a2b9d6d7827318289
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-05-19 02:43:58 +0300

    create visit model

--- TrainBookingProject/src/Models/Visit.java
+++ TrainBookingProject/src/Models/Visit.java
@@ -1,4 +1,22 @@
 package Models;
 
 public class Visit {
+    private Trip trip;
+    private City city;
+
+    public Trip getTrip() {
+        return trip;
+    }
+
+    public void setTrip(Trip trip) {
+        this.trip = trip;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
 }
