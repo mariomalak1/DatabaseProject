c16eb71611d071d51e3212f7cef6db456325bbcc
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-05-20 02:06:50 +0300

    create train controller

--- TrainBookingProject/src/Controllers/TrainController.java
+++ TrainBookingProject/src/Controllers/TrainController.java
@@ -1,4 +1,52 @@
 package Controllers;
 
+import Models.Train;
+import Models.Trip;
+import Repositories.TrainRepository;
+
+import java.util.List;
+
 public class TrainController {
+    public static Train createTrain(int capacity, Trip trip, double price){
+        try{
+            Train train = new Train(0, capacity, trip, price);
+            train = new TrainRepository().createTrain(train);
+            return train;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static boolean updateTrain(Train UpdatedTrain){
+        try{
+            new TrainRepository().UpdateTrain(UpdatedTrain);
+            return true;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static Train getTrainById(int trainId){
+        try{
+            return new TrainRepository().getTrainById(trainId);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static List<Train> getAllTrainsInTrip(int tripId){
+        try{
+            return new TrainRepository().getAllTrainsInTrip(tripId);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
