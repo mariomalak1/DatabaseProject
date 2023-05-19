5f8959782ae264ae22cb968a999363acefad0135
unknown <joerefaat22@gmail.com>
2023-05-19 04:21:13 +0300

    Implement User Model And UserRepo

--- TrainBookingProject/src/Main.java
+++ TrainBookingProject/src/Main.java
@@ -1,11 +1,30 @@
 import Models.*;
 import Repositories.BookingRepository;
 import Repositories.MainRepository;
+import Repositories.UserRepository;
+
 import java.awt.print.Book;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        MainRepository.getConnection();
+        User user = new User("Joe","Refaat","joe12324","Admin","joe22@gmail.com");
+        UserRepository u = new UserRepository();
+        try {
+            user = u.insertUser(user);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+//        try {
+//            u.deleteUser(3);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//        try {
+//            User user1 = u.getUserById(2);
+//            System.out.println(user1.getFirstName()+" "+user1.getLastName());
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
--- TrainBookingProject/src/Repositories/UserRepository.java
+++ TrainBookingProject/src/Repositories/UserRepository.java
@@ -1,4 +1,84 @@
 package Repositories;
+import Models.User;
+import java.sql.*;
 
 public class UserRepository {
+    private final Connection connection;
+    public UserRepository() {
+        this.connection = MainRepository.getConnection();
+    }
+    public User insertUser(User user) throws SQLException {
+        String sql = "INSERT INTO [User](FirstName,LastName,UserPassword,Email,Role)"+
+                "VALUES(?,?,?,?,?)";
+        try(PreparedStatement statement =connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS))
+        {
+            statement.setString(1,user.getFirstName());
+            statement.setString(2,user.getLastName());
+            statement.setString(3,user.getPassword());
+            statement.setString(4,user.getEmail());
+            statement.setString(5,user.getRole());
+            int rowAffected = statement.executeUpdate();
+            if(rowAffected == 0)
+            {
+                throw new SQLException("Insert Failed, No Row Affected ");
+            }
+            try(ResultSet generatedKey = statement.getGeneratedKeys())
+            {
+                if(generatedKey.next())
+                {
+                    int genID = generatedKey.getInt(1);
+                    user.setID(genID);
+                }
+                else
+                {
+                    throw new SQLException("Insert Failed, No ID obtained");
+                }
+            }
+        }
+        return user;
+    }
+    public void updateUser(User user) throws SQLException{
+        String sql = "UPDATE [User] SET FirstName = ?, LastName = ?, UserPassword = ?, Email = ?, Role = ? WHERE UserID = ?";
+        try (PreparedStatement statement =connection.prepareStatement(sql)){
+            statement.setString(1,user.getFirstName());
+            statement.setString(2,user.getLastName());
+            statement.setString(3,user.getPassword());
+            statement.setString(4,user.getEmail());
+            statement.setString(5,user.getRole());
+            statement.setInt(6,user.getID());
+            statement.executeUpdate();
+        }
+    }
+    public void deleteUser(int userId) throws SQLException{
+        String sql = "DELETE From [User] WHERE UserID = ?";
+        try(PreparedStatement statement = connection.prepareStatement(sql)){
+            statement.setInt(1,userId);
+            statement.executeUpdate();
+        }
+    }
+    public User getUserById(int userId) throws SQLException{
+        String sql = "SELECT * FROM [User] WHERE UserID = ?";
+        User user = null;
+        try (PreparedStatement statement = connection.prepareStatement(sql)){
+            statement.setInt(1,userId);
+            try (ResultSet resultSet = statement.executeQuery()){
+                if(resultSet.next())
+                {
+                    user = extractUserFromResultSet(resultSet);
+                }
+            }
+        }
+        return user;
+    }
+    private User extractUserFromResultSet(ResultSet r) throws SQLException{
+        int userId = r.getInt("UserID");
+        String fname = r.getString("FirstName");
+        String lname = r.getString("LastName");
+        String email = r.getString("Email");
+        String pass = r.getString("UserPassword");
+        String role = r.getString("Role");
+        User user = new User(fname,lname,pass,role,email);
+        user.setID(userId);
+        return user;
+    }
 }
